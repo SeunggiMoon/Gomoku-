10ecb917442d1954e92b7407f891ba9220739b70
MoonSeunggi <moon44432@naver.com>
2016-07-08 17:03:35 +0900

    v1.1

--- Gomoku++/main.cpp
+++ Gomoku++/main.cpp
@@ -9,9 +9,10 @@ struct space
 {
 	int state;
 	double weight;
+	bool checked;
 };
 
-space pos[MAX][MAX] = { 0, 1.0 };
+space pos[MAX][MAX] = { 0, 1.0, false };
 int checkpos[8][2] = {
 	{ -1, 1 },
 	{ 0, 1 },
@@ -23,59 +24,78 @@ int checkpos[8][2] = {
 	{ -1, 0 }
 };
 
+int lncheckpos[4][2] = {
+	{ -1, 1 },
+	{ 0, 1 },
+	{ 1, 1 },
+	{ 1, 0 },
+};
+
+int record[361][2] = { 0 };
+
 void AI()
 {
-	for (int i = 0; i < MAX; i++)
+	for (int k = 0; k < 4; k++)
 	{
+		for (int i = 0; i < MAX; i++)
 		for (int j = 0; j < MAX; j++)
+			pos[i][j].checked = false;
+
+		for (int i = 0; i < MAX; i++)
 		{
-			if (pos[i][j].state == 1)
+			for (int j = 0; j < MAX; j++)
 			{
-				for (int k = 0; k < 8; k++)
-				{
-					if (i + checkpos[k][0] >= 0 && j + checkpos[k][1] >= 0 && i + checkpos[k][0] <= MAX && j + checkpos[k][1] <= MAX)
-						pos[i + checkpos[k][0]][j + checkpos[k][1]].weight *= 1.05;
-				}
-				for (int k = 0; k < 8; k++)
+				if (pos[i][j].state == 1 && pos[i][j].checked == false)
 				{
 					int cnt = 0;
 					int tx = i, ty = j;
 					while (pos[tx][ty].state == 1 &&
-					tx >= 0 && ty >= 0 && tx <= MAX && ty <= MAX)
+						tx >= 0 && ty >= 0 && tx <= MAX && ty <= MAX)
 					{
 						cnt++;
+						pos[tx][ty].checked = true;
 						tx += checkpos[k][0];
 						ty += checkpos[k][1];
 					}
 					if (i - checkpos[k][0] >= 0 && j - checkpos[k][1] >= 0 && i - checkpos[k][0] <= MAX && j - checkpos[k][1] <= MAX)
-						for (int l = 0; l < cnt; l++) pos[i - checkpos[k][0]][j - checkpos[k][1]].weight *= pow(3.2, (cnt + 1));
+					{
+						if (pos[i - checkpos[k][0]][j - checkpos[k][1]].state == 0 &&
+							pos[tx + checkpos[k][0]][ty + checkpos[k][1]].state == 0) pos[i - checkpos[k][0]][j - checkpos[k][1]].weight *= pow(2.0, cnt * cnt);
+						else pos[i - checkpos[k][0]][j - checkpos[k][1]].weight *= pow(1.7, cnt * cnt);
+					}
 					if (tx + checkpos[k][0] >= 0 && ty + checkpos[k][1] >= 0 && tx + checkpos[k][0] <= MAX && ty + checkpos[k][1] <= MAX)
-						for (int l = 0; l < cnt; l++) pos[tx][ty].weight *= pow(3.2, (cnt + 1));
+					{
+						if (pos[i - checkpos[k][0]][j - checkpos[k][1]].state == 0 &&
+							pos[tx + checkpos[k][0]][ty + checkpos[k][1]].state == 0) pos[tx][ty].weight *= pow(2.0, cnt * cnt);
+						else pos[tx][ty].weight *= pow(1.7, cnt * cnt);
+					}
 				}
-			}
 
-			if (pos[i][j].state == 2)
-			{
-				for (int k = 0; k < 8; k++)
+				if (pos[i][j].state == 2 && pos[i][j].checked == false)
 				{
-					if (i + checkpos[k][0] >= 0 && j + checkpos[k][1] >= 0 && i + checkpos[k][0] <= MAX && j + checkpos[k][1] <= MAX)
-						pos[i + checkpos[k][0]][j + checkpos[k][1]].weight *= 1.02;
-				}
-				for (int k = 0; k < 8; k++)
-				{
+					for (int l = 0; l < 8; l++)
+					{
+						if (i + checkpos[l][0] >= 0 && j + checkpos[l][1] >= 0 && i + checkpos[l][0] <= MAX && j + checkpos[l][1] <= MAX)
+							pos[i + checkpos[l][0]][j + checkpos[l][1]].weight *= 1.05;
+					}
+
 					int cnt = 0;
 					int tx = i, ty = j;
 					while (pos[tx][ty].state == 2 &&
 						tx >= 0 && ty >= 0 && tx <= MAX && ty <= MAX)
 					{
 						cnt++;
+						pos[tx][ty].checked = true;
 						tx += checkpos[k][0];
 						ty += checkpos[k][1];
 					}
-					if (i - checkpos[k][0] >= 0 && j - checkpos[k][1] >= 0 && i - checkpos[k][0] <= MAX && j - checkpos[k][1] <= MAX)
-						for (int l = 0; l < cnt; l++) pos[i - checkpos[k][0]][j - checkpos[k][1]].weight *= pow(2.0, (cnt));
-					if (tx + checkpos[k][0] >= 0 && ty + checkpos[k][1] >= 0 && tx + checkpos[k][0] <= MAX && ty + checkpos[k][1] <= MAX)
-					for (int l = 0; l < cnt; l++) pos[tx][ty].weight *= pow(2.0, (cnt));
+					if (cnt > 1)
+					{
+						if (i - checkpos[k][0] >= 0 && j - checkpos[k][1] >= 0 && i - checkpos[k][0] <= MAX && j - checkpos[k][1] <= MAX)
+							pos[i - checkpos[k][0]][j - checkpos[k][1]].weight *= pow(1.4, cnt * cnt);
+						if (tx + checkpos[k][0] >= 0 && ty + checkpos[k][1] >= 0 && tx + checkpos[k][0] <= MAX && ty + checkpos[k][1] <= MAX)
+							pos[tx][ty].weight *= pow(1.4, cnt * cnt);
+					}
 				}
 			}
 		}
@@ -90,12 +110,11 @@ void gotoxy(int x, int y)
 
 int main(void)
 {
-
-	int x, y, ex, ey, tx, ty, cnt;
+	int x, y, ex, ey, tx, ty, cnt, putcnt;
 	bool put, p1win, p2win;
 	double max_weight;
 
-	printf("\n Gomoku++ v1.0\n\n ⓒ 2016 Naissoft all rights reserved.\n\n 조작키 : w, s, a, d, 돌 놓기 Space\n");
+	printf("\n Gomoku++ v1.1\n\n ⓒ 2016 Naissoft all rights reserved.\n\n 조작키 : w, s, a, d, 돌 놓기 Space\n");
 	printf("\n 시작하려면 Enter를 누르세요.");
 	getchar();
 
@@ -107,6 +126,8 @@ int main(void)
 		for (int j = 0; j < MAX; j++)
 			pos[i][j].state = 0, pos[i][j].weight = 1.0;
 
+		for (int i = 0; i < 361; i++) record[i][0] = record[i][1] = 0;
+
 		for (int i = 0; i < MAX * 2; i += 2)
 		{
 			for (int j = 0; j < MAX; j++)
@@ -114,7 +135,7 @@ int main(void)
 			printf("\n");
 		}
 
-		x = y = ex = ey = tx = ty = 0;
+		x = y = ex = ey = tx = ty = putcnt = 0;
 		p1win = p2win = false;
 		max_weight = 0.0;
 
@@ -147,6 +168,8 @@ int main(void)
 				case ' ':
 					if (pos[x][y].state == 0)
 					{
+						record[putcnt][0] = x, record[putcnt][1] = y;
+						putcnt++;
 						pos[x][y].state = 1;
 						printf("●");
 						gotoxy(x * 2, y);
@@ -154,6 +177,8 @@ int main(void)
 					}
 					break;
 				}
+				gotoxy(1, 21); printf(" Weight : %f                                   ", pos[x][y].weight);
+				gotoxy(x * 2, y);
 			}
 
 			put = false;
@@ -211,10 +236,12 @@ int main(void)
 						}
 					}
 				}
-				
+
 				if (pos[ex][ey].state == 0)
 				{
 					pos[ex][ey].state = 2;
+					record[putcnt][0] = ex, record[putcnt][1] = ey;
+					putcnt++;
 					gotoxy(ex * 2, ey);
 					printf("○");
 					put = true;
@@ -255,10 +282,33 @@ int main(void)
 		Sleep(1000);
 		gotoxy(1, 20);
 		printf(" %s가 우승했습니다!", (p1win > p2win) ? "플레이어" : "AI");
-		printf("\n\n 다시 하시겠습니까? Y / N");
+		printf("\n\n 다시 하시겠습니까? Y / N, 리플레이 : R");
 
-		if (getch() == 'N') break;
-		else system("cls");
+		char ch;
+		scanf(" %c", &ch);
+
+		if (ch == 'N') break;
+		else if (ch == 'Y') system("cls");
+		else if (ch == 'R')
+		{
+			system("cls");
+			for (int i = 0; i < MAX * 2; i += 2)
+			{
+				for (int j = 0; j < MAX; j++)
+					gotoxy(i, j), printf("·");
+				printf("\n");
+			}
+
+			for (int i = 0; i < putcnt; i++)
+			{
+				gotoxy(record[i][0] * 2, record[i][1]);
+				if (pos[record[i][0]][record[i][1]].state == 1)
+					printf("●");
+				else if (pos[record[i][0]][record[i][1]].state == 2)
+					printf("○");
+				getch();
+			}
+		}
 	}
 	return 0;
 }
